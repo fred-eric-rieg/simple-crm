251d3913fcf999908ac0709404d904d18e22bef6
Frederic Rieg <fredericrieg@protonmail.com>
2023-11-13 09:31:05 +0300

    added mainpage and material design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainpageComponent } from './components/mainpage/mainpage.component';
+
+const routes: Routes = [
+  { path: '', component: MainpageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/mainpage/mainpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-mainpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mainpage.component.html',
+  styleUrl: './mainpage.component.scss'
+})
+export class MainpageComponent {
+
+}
